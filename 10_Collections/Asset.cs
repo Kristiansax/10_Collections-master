3f7edfa7699ef6475ea07c6295833b30db6da483
Kristiansax <Kristiansax@KRELLES_VISION>
2016-10-03 11:08:50 +0200

    Add project files.

---
+++ 10_Collections/Asset.cs
@@ -0,0 +1,10 @@
+﻿namespace _10_Collections
+{
+    public interface IAsset
+    {
+        int Id {get;}
+
+        double GetValue();
+        string GetSymbol();
+    }
+}
---
+++ 10_Collections/StockNameComparator.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+
+namespace _10_Collections
+{
+    public class StockNameComparator : IComparer<IAsset>
+    {
+        public int Compare(IAsset x, IAsset y)
+        {
+            return x.GetSymbol().CompareTo(y.GetSymbol());           
+        }
+
+    }
+}
